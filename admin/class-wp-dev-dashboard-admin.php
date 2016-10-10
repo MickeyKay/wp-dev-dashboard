481a2edb26ba59d0b09baa206e473606896af246
Greg Ross <greg@toolstack.com>
2016-10-10 11:51:07 -0400

    Use the proper namespace for the dom parser.

--- admin/class-wp-dev-dashboard-admin.php
+++ admin/class-wp-dev-dashboard-admin.php
@@ -1,4 +1,5 @@
 <?php
+use Sunra\PhpSimple\HtmlDomParser;
 
 /**
  * The dashboard-specific functionality of the plugin.
@@ -1006,7 +1007,7 @@ class WP_Dev_Dashboard_Admin {
 			return false;
 		}
 
-		$html = str_get_html( $html );
+		$html = HtmlDomParser::str_get_html( $html );
 
 		$table = $html->find( 'li[class=bbp-body]', 0 );
 
