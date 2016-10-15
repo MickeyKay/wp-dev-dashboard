5c9e613b8940d76a1def579a06ccc25cd461df94
Greg Ross <greg@toolstack.com>
2016-10-15 15:03:13 -0400

    Support minified version of css/js files with WP_DEBUG.

--- admin/class-wp-dev-dashboard-admin.php
+++ admin/class-wp-dev-dashboard-admin.php
@@ -170,8 +170,10 @@ class WP_Dev_Dashboard_Admin {
 	 */
 	public function enqueue_styles() {
 
-		wp_enqueue_style( $this->plugin_slug, plugin_dir_url( __FILE__ ) . 'css/wp-dev-dashboard-admin.css', array(), $this->version, 'all' );
-		wp_enqueue_style( 'jquery-table-sorter-css', plugin_dir_url( __FILE__ ) . 'css/jquery-table-sorter.css', array(), $this->version, 'all' );
+		$suffix = SCRIPT_DEBUG ? '.css' : '.min.css';
+
+		wp_enqueue_style( $this->plugin_slug, plugin_dir_url( __FILE__ ) . 'css/wp-dev-dashboard-admin' . $suffix, array(), $this->version, 'all' );
+		wp_enqueue_style( 'jquery-table-sorter-css', plugin_dir_url( __FILE__ ) . 'css/jquery-table-sorter' . $suffix, array(), $this->version, 'all' );
 
 	}
 
@@ -182,8 +184,10 @@ class WP_Dev_Dashboard_Admin {
 	 */
 	public function enqueue_scripts() {
 
-		wp_enqueue_script( $this->plugin_slug, plugin_dir_url( __FILE__ ) . 'js/wp-dev-dashboard-admin.js', array( 'jquery' ), $this->version, true );
-		wp_enqueue_script( 'jquery-tablesorter-js', plugin_dir_url( __FILE__ ) . 'js/jquery.tablesorter.min.js', array( 'jquery' ), $this->version, true );
+		$suffix = SCRIPT_DEBUG ? '.js' : '.min.js';
+
+		wp_enqueue_script( $this->plugin_slug, plugin_dir_url( __FILE__ ) . 'js/wp-dev-dashboard-admin' . $suffix, array( 'jquery' ), $this->version, true );
+		wp_enqueue_script( 'jquery-tablesorter-js', plugin_dir_url( __FILE__ ) . 'js/jquery.tablesorter' . $suffix, array( 'jquery' ), $this->version, true );
 
 		wp_localize_script( $this->plugin_slug, "wpddSettings", $this->js_data );
 
